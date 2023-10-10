ca93cca918c5efcca678acf5d5358feabeb32ca4
Req <justreqofficial@gmail.com>
2023-10-10 02:12:41 -0400

    Break over

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 const getJsonData = (localPath) => {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", localPath, false);
@@ -9,17 +11,29 @@ const getJsonData = (localPath) => {
 const Data = getJsonData("./data.json");
 const homeExperienceGridElements = Array.from(document.getElementById("home-experience-grid").children);
 
-homeExperienceGridElements.forEach(x => {
-    x.addEventListener("mouseover", (event) => {
+homeExperienceGridElements.forEach(e => {
+    e.addEventListener("mouseover", (event) => {
         const target = event.currentTarget;
+
+        if (!target.firstElementChild.classList.contains("aos-animate")) return;
+
         const experience = Data.experience[target.firstChild.alt];
 
-        homeExperienceGridElements.forEach(y => {
-            y.style.backgroundColor = y == target ? "rgb(31 41 55)" : "transparent";
+        homeExperienceGridElements.forEach(x => {
+            x.style.backgroundColor = x == target ? "rgb(31 41 55)" : "transparent";
         });
 
         document.getElementById("home-experience-proficiency-bar").lastElementChild.style.width = `${10 - experience.proficiency}0%`;
         document.getElementById("home-experience-started-using-text").innerText =
             `Started using ${moment(experience.startedUsing, "MM-YYYY").fromNow()}\n\nI've used ${experience.name} for:\n• ${experience.uses.join("\n• ")}`;
+    });
+
+    e.addEventListener("mouseout", (event) => {
+        homeExperienceGridElements.forEach(x => {
+            x.style.backgroundColor = "transparent";
+        });
+
+        document.getElementById("home-experience-proficiency-bar").lastElementChild.style.width = `100%`;
+        document.getElementById("home-experience-started-using-text").innerText = "Hover over an icon to learn more";
     });
 });
